9b6534bdce1e0f73a8b81dff98ab7cb49df22cf0
link <719656634@qq.com>
2020-10-09 17:15:15 +0800

    完成了后台分类列表curd功能

--- admin/src/router/router.js
+++ admin/src/router/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit'
+import CategoryList from '../views/CategoryList'
 
 Vue.use(VueRouter)
 
@@ -10,7 +11,9 @@ const routes = [{
         name: 'main',
         component: Main,
         children: [
-            {path:'/categories/create', component: CategoryEdit}
+            {path:'/categories/create', component: CategoryEdit},
+            {path:'/categories/edit/:id', component: CategoryEdit, props: true},
+            {path:'/categories/list', component: CategoryList},
         ]
     },
 
--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="about">
-    <h1>创建分类</h1>
+    <h1>{{id ? '编辑' : '创建'}}分类 </h1>
     <el-form label-width="120px" @submit.native.prevent="save">
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type= 'primary' navtive-type='submit'>保存</el-button>
+        <el-button type= 'primary' native-type='submit'>保存</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -14,15 +14,32 @@
 
 <script>
   export default {
+    props: {
+      id: {}
+    },
     data() {
       return {
         model: {}
       }
     },
     methods: {
-      save() {
-        
+      async save() {
+        let res = res
+        if (this.id) {
+          res = await this.$http.put(`categories/${this.id}`, this.model)
+        } else {
+          res = await this.$http.post('categories', this.model)
+        }
+         this.$router.push('/categories/list') 
+         this.$message.success('保存成功')
+      },
+      async fetch() {
+        const res = await this.$http.get(`categories/${this.id}`)
+        this.model = res.data
       }
+    },
+    created() {
+      this.id && this.fetch()
     },
   }
 </script>
