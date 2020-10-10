807d09e599285b029b1e5afbe2a1560db0d6d747
link <719656634@qq.com>
2020-10-10 17:39:35 +0800

    完成装备CURD

--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -2,9 +2,18 @@
   <div class="about">
     <h1>{{id ? '编辑' : '创建'}}物品 </h1>
     <el-form label-width="120px" @submit.native.prevent="save">
-      
+
       <el-form-item label="名称"><el-input v-model="model.name"></el-input></el-form-item>
-      <el-form-item label="图标"><el-input v-model="model.icon"></el-input></el-form-item>
+      <el-form-item label="图标">
+        <el-upload
+          class="avatar-uploader"
+          :action="$http.defaults.baseURL + '/upload'"
+          :show-file-list="false"
+          :on-success="afterUpload">
+          <img v-if="model.icon" :src="model.icon" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+</el-form-item>
       <el-form-item>
         <el-button type= 'primary' native-type='submit'>保存</el-button>
       </el-form-item>
@@ -24,6 +33,10 @@
       }
     },
     methods: {
+      afterUpload (res) {
+        this.$set(this.model, 'icon', res.url)
+        // this.model.icon = res.url
+      },
       async save() {
         let res = res
         if (this.id) {
@@ -50,5 +63,27 @@
 </script>
 
 <style>
-
+ .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
